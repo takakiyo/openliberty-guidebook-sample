38f8515e54d054cd5fd8e109c0eb9a76c4ea36ed
Takakiyo Tanaka <t.takakiy@gmail.com>
2023-11-30 15:44:49 +0900

    とりあえず，現状をCommit

---
+++ maven-sample/src/main/java/com/example/maven/sample/MavensampleRestApplication.java
@@ -0,0 +1,11 @@
+package com.example.maven.sample;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+/**
+ *
+ */
+@ApplicationPath("/data")
+public class MavensampleRestApplication extends Application {
+}
